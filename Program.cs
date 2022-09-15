a9edd4f7e45dc6e9ed581c2f3a2a7657f21d565e
Cix8 <francesco.natal_fn@libero.it>
2022-09-15 15:18:10 +0200

    first commit: add myFoods_array, myFoods.Length printed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Definizione ed Inizializzazione dell'array "myFoods"
+string[] myFoods = { "Lasagne", "Carbonara", "Cotoletta", "Patate fritte", "Hamburger", "Sushi", "Cornetto salato", "Barbabietola" };
+
+//Calcolo e stampa della lunghezza dell'array
+Console.WriteLine(myFoods.Length);
