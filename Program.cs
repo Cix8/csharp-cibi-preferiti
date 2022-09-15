d8d0f1b41d5451357baa6a45c958afc1ef46fbbb
Cix8 <francesco.natal_fn@libero.it>
2022-09-15 16:47:43 +0200

    "Barbabietola" re-added  in myFoods

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // Definizione ed Inizializzazione dell'array "myFoods"
-string[] myFoods = { "Lasagne", "Carbonara", "Cotoletta", "Patate fritte", "Hamburger", "Sushi", "Cornetto salato" };
+string[] myFoods = { "Lasagne", "Carbonara", "Cotoletta", "Patate fritte", "Hamburger", "Sushi", "Cornetto salato", "Barbabietola" };
 
 //Calcolo e stampa della lunghezza dell'array
 Console.WriteLine(myFoods.Length);
